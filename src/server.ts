8ede4495b8443ac93ed026dde0cd7408b19b3b39
Flávio Arêas <vflavio9@gmail.com>
2022-07-02 20:25:52 -0300

    feat: create handleDownload method

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
-import bcrypt from 'bcrypt';
 import 'dotenv/config';
-import express from 'express';
-import mongoose from 'mongoose';
+import express, { Request, Response } from 'express';
 import multer from 'multer';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
 import { File } from './models/File';
 
 const PORT = process.env.PORT || 3333;
 const DATABASE_URL = process.env.DATABASE_URL || '';
+const APP_URL = process.env.APP_URL || '';
 
 const server = express();
+server.use(express.urlencoded({ extended: true }));
 
 const upload = multer({
   dest: 'uploads',
@@ -28,15 +31,42 @@ server.post('/upload', upload.single('file'), async (req, res) => {
     originalName: req.file?.originalname,
   };
 
-  if (password !== null && password !== '') {
+  if (password && password.trim() !== '') {
+    const hashPassword = await bcrypt.hash(password, 10);
+
     Object.assign(fileData, {
-      password: await bcrypt.hash(password, 10),
+      password: hashPassword,
     });
   }
 
   const file = await File.create(fileData);
 
-  return res.json(file.originalName);
+  return res.json({ fileLink: `${APP_URL}/file/${file.id}` });
 });
+
+server.route('/file/:id').get(handleDownload).post(handleDownload);
+
+async function handleDownload(req: Request, res: Response) {
+  const { password } = req.body;
+
+  const file = await File.findById(req.params.id);
+
+  if (!file) {
+    return res.status(400).json({ error: 'File not found' });
+  }
+
+  if (!password) {
+    return res.status(400).json({ error: 'No password was provided.' });
+  }
+
+  if (file.password && !(await bcrypt.compare(password, file.password))) {
+    return res.status(400).json({ error: 'Wrong password.' });
+  }
+
+  file.downloadCounter++;
+  await file.save();
+
+  return res.download(file.path, file.originalName);
+}
 
 server.listen(PORT, () => console.log(`💻 Server is up in port... ${PORT}`));
