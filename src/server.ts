9bed737f19173b3f6990c33fc21fa524b7e534f5
Flávio Arêas <vflavio9@gmail.com>
2022-07-02 18:46:11 -0300

    chore: set up multer and mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
+import multer from 'multer';
+
+const PORT = process.env.PORT || 3333;
+const DATABASE_URL = process.env.DATABASE_URL || '';
 
 const server = express();
 
-const PORT = process.env.PORT || 3333;
+const upload = multer({
+  dest: 'uploads',
+});
+
+mongoose.connect(DATABASE_URL);
 
 server.get('/', (req, res) => {
-  res.json({ hello: 'World' });
+  return res.json({ hello: 'World' });
+});
+
+server.get('/upload', upload.single('file'), (req, res) => {
+  return res.json({ upload: 'File' });
 });
 
 server.listen(PORT, () => console.log(`💻 Server is up in port... ${PORT}`));
